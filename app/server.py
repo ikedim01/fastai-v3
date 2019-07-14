71b1104c0e09755fb8794d9527c9ffbe3c0cb3b7
ikedim01 <ikedim@gmail.com>
2019-07-13 22:27:47 -0400

    poodle test

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/5p5d4emazkip73a/poodle.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+#classes = ['black', 'grizzly', 'teddys']
+classes = ['notpoodle', 'poodle']
 path = Path(__file__).parent
 
 app = Starlette()
